e22da862e2e3d80616efa3bcc7188b745e6eef8b
V. Vallés <vivafeno@gmail.com>
2025-01-05 16:08:43 +0100

    [ Sección 14 ] Menú Héroes + JSonServer y datos ficticios

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,6 +1,9 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+
 import { AuthRoutingModule } from './auth-routing.module';
+import { MaterialModule } from '../material/material.module';
+
 import { LayoutPageComponent } from './pages/layout-page/layout-page.component';
 import { LoginPageComponent } from './pages/login-page/login-page.component';
 import { RegisterPageComponent } from './pages/register-page/register-page.component';
@@ -16,6 +19,7 @@ import { RegisterPageComponent } from './pages/register-page/register-page.compo
   imports: [
     CommonModule,
     AuthRoutingModule,
+    MaterialModule,
 
   ]
 })
--- src/app/heroes/pages/layout-page/layout-page.component.ts
+++ src/app/heroes/pages/layout-page/layout-page.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-layout-page',
   standalone: false,
   
   templateUrl: './layout-page.component.html',
-  styles: ``
+  styles: `
+  .custom-toolbar {
+  background-color: #ff5722; /* Cambia este valor al color que desees */
+}
+  `
 })
 export class LayoutPageComponent {
 
+  public sidebarItems = [
+    {label: 'Listado', icon: 'list', url: './list'},
+    {label: 'Añadir', icon: 'add', url: './new-hero'},
+    {label: 'Buscar', icon: 'search', url: './search'}
+  ];
 }
